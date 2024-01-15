44c5bf4c2d4610724c0e67c7718f4d13ffda7cd8
Harsh Keshri <harshkeshri1234567@gmail.com>
2024-01-15 13:00:01 +0530

    function

--- common_programming/functions/src/main.rs
+++ common_programming/functions/src/main.rs
@@ -1,3 +1,34 @@
 fn main() {
-    println!("Hello, world!");
+    another_function();
+
+    let mut n: u32 = 9;
+
+    parameter(n);
+
+    println!("{}", n);
+
+    n = parameter_return(n);
+
+    println!("{}", n);
+
+    {
+        let m = 3;
+
+        println!("Bracket {}", m);
+    }
+}
+
+fn another_function() {
+    println!("Another")
+}
+
+fn parameter(mut n: u32) {
+    n = n + 1;
+    println!("Inside Param Function {}", n);
+}
+
+fn parameter_return(mut n: u32) -> u32 {
+    n = n + 1;
+    println!("Inside Param Return Function {}", n);
+    n
 }
